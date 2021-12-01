ab752b1b9feac554d6e1617b0193461bdd1c513e
Alexey Kalinin <alexey.kalinin@passportinc.com>
2021-12-01 12:26:28 -0500

    Added second task for Day 1

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ const fs = require('fs');
 
 const depths = fs.readFileSync("./input.txt").toString().split("\n");
 
-const increasedArray = depths.filter((element, index, array)=>{
-    return parseInt(element) > parseInt(array[index-1]) && index!==0;
-})
+const convertToIncreasedArray = (arr) => {
+    return arr.filter((element, index, array) => {
+        return element > array[index - 1] && index !== 0;
+    })
+}
+
+const convertToArrayofIntegers = (arr) => {
+    return arr.map(element => parseInt(element));
+}
+
+const convertToFloatingSumsArray = (arr) => {
+    return arr.map((element, index, array) => {
+        if (index < array.length - 2) {
+            return element + array[index + 1] + array[index + 2]
+        } else {
+            return 0;
+        }
+    })
+}
+
+// Day 1 - Part 1
+const increasedArray = convertToIncreasedArray(convertToArrayofIntegers(depths));
 
 console.log(increasedArray.length);
+
+// Day 1 - Part 2
+const increasedSumArray = convertToIncreasedArray(convertToFloatingSumsArray(convertToArrayofIntegers(depths)));
+
+console.log(increasedSumArray.length);
